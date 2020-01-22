3c2f38a10b5efd0ce5a6aa9e391840b3ed2226d5
ZianeA <wit_wit90@hotamail.com>
2020-01-22 17:30:25 +0100

    Remove auto rotation when app starts

---
+++ Assets/ScreenOrientationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenOrientationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var canvasRect = transform.GetComponent<RectTransform>().rect;
+        if (canvasRect.width > canvasRect.height)
+        {
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+        }
+        else
+        {
+            Screen.orientation = ScreenOrientation.Portrait;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
